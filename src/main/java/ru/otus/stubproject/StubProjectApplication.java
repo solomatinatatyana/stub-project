ef596c93fd5c09c1d788ebaeda747c5b93552b3a
Tatiana <tokio9507@mail.ru>
2020-05-07 19:17:42 +0300

    Initial commit

---
+++ src/main/java/ru/otus/stubproject/StubProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.stubproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StubProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StubProjectApplication.class, args);
+    }
+
+}
